1a892665af0ebff4458d82563bca5ebf6c78a7d2
HannahImo <Preetyann21@gmail.com>
2023-05-19 22:14:09 +0100

    Added css

--- blog/src/dummy/Card.jsx
+++ blog/src/dummy/Card.jsx
@@ -1,4 +1,4 @@
-import Maxup from '../dummy/Maxup'; 
+ 
 
  function Card () {
   return (
@@ -8,22 +8,81 @@ import Maxup from '../dummy/Maxup';
       <div>
       {/* step 1: write your card content and duplicate it. */}
       
-      <div className="cardsmple">
-        <div class="card-body">
-           <h3 class="card-title">Starter pack</h3>
-           <h4 class="card-subtitle mb-2 text-body-secondary">$0</h4>
-           <h5 class="card-title">Features</h5>
-             <ul>
-               <li> Free hosting </li>
-               <li> Free web domain </li>
-             </ul>
-             <Maxup/>
-               <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-               <button onClick={Card}> Signup </button>
-                   </div>
-                   </div>
+      <div class="d-flex justify-content-center"><div class="card">  
+        <div className="cardsmple" class="card bg-light mb-3" style= {{maxwidth: '18rem'}}>
+          <div class="card-body">
+     <h3 class="card-title">Starter pack</h3>
+     <h4 class="card-subtitle mb-2 text-body-secondary">$0</h4>
+     <h5 class="card-title">Features</h5>
+       <ul className="card-list" >
+         <li> <img width="24" height="24" src="https://img.icons8.com/material-outlined/24/checked--v1.png" alt="checked--v1"/>Free hosting </li>
+         <li> <img width="24" height="24" src="https://img.icons8.com/material-outlined/24/checked--v1.png" alt="checked--v1"/>Free web domain </li>
+         <li> <img width="24" height="24" src="https://img.icons8.com/material-outlined/24/checked--v1.png" alt="checked--v1"/>Free web security</li>
+         <li> <img width="24" height="24" src="https://img.icons8.com/material-outlined/24/checked--v1.png" alt="checked--v1"/>Free database for 1 month </li>
+       </ul>
+         
+             <a href="/" class="card-link">Get started</a>
+             </div>
+             </div>
+      </div>
           </div>
           </div>
+
+
+
+          <div>
+      <div>
+      {/* step 1: write your card content and duplicate it. */}
+      
+      <div class="d-flex justify-content-center"><div class="card">  
+        <div className="cardsmple" class="card bg-success mb-3" style= {{maxwidth: '18rem'}}>
+          <div class="card-body">
+     <h3 class="card-title">Gold pack</h3>
+     <h4 class="card-subtitle mb-2 text-body-secondary">$20</h4>
+     <h5 class="card-title">Features</h5>
+       <ul className="card-list" >
+         <li> <img width="24" height="24" src="https://img.icons8.com/material-outlined/24/checked--v1.png" alt="checked--v1"/>Free hosting </li>
+         <li> <img width="24" height="24" src="https://img.icons8.com/material-outlined/24/checked--v1.png" alt="checked--v1"/>Free web domain </li>
+         <li> <img width="24" height="24" src="https://img.icons8.com/material-outlined/24/checked--v1.png" alt="checked--v1"/>Free web security</li>
+         <li> <img width="24" height="24" src="https://img.icons8.com/material-outlined/24/checked--v1.png" alt="checked--v1"/>Free database for 1 month </li>
+       </ul>
+         
+             <a href="/" class="card-link">Get started</a>
+             </div>
+             </div>
+      </div>
+          </div>
+        </div>
+
+
+
+          <div>
+      <div>
+      {/* step 1: write your card content and duplicate it. */}
+      
+      <div class="d-flex justify-content-center"><div class="card">  
+        <div className="cardsmple" class="card bg-primary mb-3" style= {{maxwidth: '18rem'}}>
+          <div class="card-body">
+     <h3 class="card-title">Diamond pack</h3>
+     <h4 class="card-subtitle mb-2 text-body-secondary">$50</h4>
+     <h5 class="card-title">Features</h5>
+       <ul className="card-list" >
+         <li> <img width="24" height="24" src="https://img.icons8.com/material-outlined/24/checked--v1.png" alt="checked--v1"/>Free hosting </li>
+         <li> <img width="24" height="24" src="https://img.icons8.com/material-outlined/24/checked--v1.png" alt="checked--v1"/>Free web domain </li>
+         <li> <img width="24" height="24" src="https://img.icons8.com/material-outlined/24/checked--v1.png" alt="checked--v1"/>Free web security</li>
+         <li> <img width="24" height="24" src="https://img.icons8.com/material-outlined/24/checked--v1.png" alt="checked--v1"/>Free database for 1 month </li>
+       </ul>
+         
+             <a href="/" class="card-link">Get started</a>
+             </div>
+             </div>
+      </div>
+          </div>
+          </div>
+          </div>
+          </div>
+          </div>
+          
     </>
   )
  }
--- blog/src/dummy/Home.jsx
+++ blog/src/dummy/Home.jsx
@@ -8,11 +8,12 @@ function Home(){
      <h3 class="card-title">Gold pack</h3>
      <h4 class="card-subtitle mb-2 text-body-secondary">$20</h4>
      <h5 class="card-title">Features</h5>
+     <img width="24" height="24" src="https://img.icons8.com/material-outlined/24/checked--v1.png" alt="checked--v1"/>
        <ul>
-         <li> Free hosting </li>
-         <li> Free web domain </li>
-         <li> Free web security</li>
-         <li> Free database for 1 month </li>
+         <li> <img width="24" height="24" src="https://img.icons8.com/material-outlined/24/checked--v1.png" alt="checked--v1"/>Free hosting </li>
+         <li> <img width="24" height="24" src="https://img.icons8.com/material-outlined/24/checked--v1.png" alt="checked--v1"/>Free web domain </li>
+         <li> <img width="24" height="24" src="https://img.icons8.com/material-outlined/24/checked--v1.png" alt="checked--v1"/>Free web security</li>
+         <li> <img width="24" height="24" src="https://img.icons8.com/material-outlined/24/checked--v1.png" alt="checked--v1"/>Free database for 1 month </li>
        </ul>
          <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
              <a href="/" class="card-link">Get started</a>
