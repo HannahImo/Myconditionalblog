f2a5360b6cafac461cc2b0d1baddf304b320fec6
ndukwesamuel <ndukwesamuel23@yahoo.com>
2023-06-11 07:04:33 +0100

    ne

--- blog/src/Land/Coaches.jsx
+++ blog/src/Land/Coaches.jsx
@@ -1,54 +1,74 @@
-import React from 'react'
+import React from "react";
+import Coaches_Component from "../Component/Coaches_Component";
+
+import styles from "../Component/Coaches.module.css";
 
 function Coaches() {
+  let Coaches_data = [
+    {
+      id: 1,
+      img: "https://w0.peakpx.com/wallpaper/207/306/HD-wallpaper-sight-see-ing-pretty-sun-beautiful-sunset-adorable-sea-sweet-nice-anime-beauty-anime-girl-sunrise-scenery-female-horizon-lovely-ocean-brown-hair-skirt-blouse-sky-short-hair-cute.jpg",
+      body: "What Critical Thinking Is—And 7 Ways to Improve Yours",
+      title: "Alyse Kalish",
+    },
+
+    {
+      id: 2,
+      img: "https://w0.peakpx.com/wallpaper/207/306/HD-wallpaper-sight-see-ing-pretty-sun-beautiful-sunset-adorable-sea-sweet-nice-anime-beauty-anime-girl-sunrise-scenery-female-horizon-lovely-ocean-brown-hair-skirt-blouse-sky-short-hair-cute.jpg",
+      body: "What Critical Thinking Is—And 7 Ways to Improve Yours",
+      title: "samheat Kalish",
+    },
+    {
+      id: 3,
+      img: "https://w0.peakpx.com/wallpaper/207/306/HD-wallpaper-sight-see-ing-pretty-sun-beautiful-sunset-adorable-sea-sweet-nice-anime-beauty-anime-girl-sunrise-scenery-female-horizon-lovely-ocean-brown-hair-skirt-blouse-sky-short-hair-cute.jpg",
+      body: "What Critical Thinking Is—And 7 Ways to Improve Yours",
+      title: "samheat Kalish",
+    },
+
+    {
+      id: 4,
+      img: "https://w0.peakpx.com/wallpaper/207/306/HD-wallpaper-sight-see-ing-pretty-sun-beautiful-sunset-adorable-sea-sweet-nice-anime-beauty-anime-girl-sunrise-scenery-female-horizon-lovely-ocean-brown-hair-skirt-blouse-sky-short-hair-cute.jpg",
+      body: "What Critical Thinking Is—And 7 Ways to Improve Yours",
+      title: "samheat Kalish",
+    },
+
+    {
+      id: 5,
+      img: "https://w0.peakpx.com/wallpaper/207/306/HD-wallpaper-sight-see-ing-pretty-sun-beautiful-sunset-adorable-sea-sweet-nice-anime-beauty-anime-girl-sunrise-scenery-female-horizon-lovely-ocean-brown-hair-skirt-blouse-sky-short-hair-cute.jpg",
+      body: "What Critical Thinking Is—And 7 Ways to Improve Yours",
+      title: "samheat Kalish",
+    },
+
+    {
+      id: 6,
+      img: "https://w0.peakpx.com/wallpaper/207/306/HD-wallpaper-sight-see-ing-pretty-sun-beautiful-sunset-adorable-sea-sweet-nice-anime-beauty-anime-girl-sunrise-scenery-female-horizon-lovely-ocean-brown-hair-skirt-blouse-sky-short-hair-cute.jpg",
+      body: "What Critical Thinking Is—And 7 Ways to Improve Yours",
+      title: "samheat Kalish",
+    },
+  ];
   return (
-    <div className='coaches-container'>
-        <div className='sub-coaches-container'>
-            <h2 className='heading'> Try Coach Connect</h2>
-            <p className='body'> Meet hundreds of certified coaches who are trained to help you improve <br></br>
-          your resume, create a strategy for your job search, and more</p>  
+    <div className="coaches-container">
+      <div className="sub-coaches-container">
+        <h2 className="heading"> Try Coach Connect</h2>
+        <p className="body">
+          {" "}
+          Meet hundreds of certified coaches who are trained to help you improve{" "}
+          <br></br>
+          your resume, create a strategy for your job search, and more
+        </p>
+      </div>
+
+      <div className="coach-content-wrapper">
+        <div className={styles["coach-ul"]}>
+          <Coaches_Component ourdata={Coaches_data} />
         </div>
-
-        <div className='coach-content-wrapper'> 
-            <ul className='coach-content'>
-                <section className='caoch-card'>
-                    <img alt='Amanda Moxham' src='/' height={'80'} width={'80'}/>
-                    <h3 className='card-title'> Amanda Moxham </h3>
-                    <p className='card-details'> Specialties: College/New Grads, New Managers,  Mid Career </p>
-                </section>
-
-                <section className='caoch-card'>
-                    <img alt='John C. Lin' src='/' height={'80'} width={'80'}/>
-                    <h3 className='card-title'> Joy C. Lin </h3>
-                    <p className='card-level'> Coach </p>
-                    <p className='card-details'> Specialties:  Mid Career, Executives, Women </p>
-                </section>
-
-                <section className='caoch-card'>
-                    <img alt='Alex Durand' src='/' height={'80'} width={'80'}/>
-                    <h3 className='card-title'> Alex Durand </h3>
-                    <p className='card-level'> Master Coach </p>
-                    <p className='card-details'> Specialties: New Manager, Mid Career, Executives </p>
-                </section>
-
-                <section className='caoch-card'>
-                    <img alt='Alex Durand' src='/' height={'80'} width={'80'}/>
-                    <h3 className='card-title'> Tamara Ellison </h3>
-                    <p className='card-level'> Mentor </p>
-                    <p className='card-details'> Specialties: New Manager, Mid Career, Executives </p>
-                </section>
-
-                <section className='caoch-card'>
-                    <img alt='Alex Durand' src='/' height={'80'} width={'80'}/>
-                    <h3 className='card-title'> Steven Davis </h3>
-                    <p className='card-level'> Coach </p>
-                    <p className='card-details'> Specialties: College/New Grads, Startups, Engineering/Tech  </p>
-                </section>
-            </ul>
-        </div>
-            <a href='/' className='btn-link'> Meet The Coaches </a>
+      </div>
+      <a href="/" className="btn-link">
+        {" "}
+        Meet The Coaches{" "}
+      </a>
     </div>
-  )
+  );
 }
 
-export default Coaches
+export default Coaches;
